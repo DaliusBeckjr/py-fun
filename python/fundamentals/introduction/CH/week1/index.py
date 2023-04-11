2530c5d531c9ea4eef6d845c11813211b5da217d
Dalius Beck <daliusbeckxiv@gmail.com>
2023-04-10 23:34:25 -0700

    just adding notes

---
+++ python/fundamentals/introduction/CH/week1/index.py
@@ -0,0 +1,47 @@
+students = [
+    {
+        'f_name' : 'Ronald',
+        'l_name' : 'don',
+        'age' : 45,
+        'dob' : 'january 22'
+    },
+    {
+        'f_name' : 'todd',
+        'l_name' : 'germo',
+        'age' : 27,
+        'dob' : 'november 16'
+    },
+    {
+        'f_name' : 'tony',
+        'l_name' : 'dents',
+        'age' : 32,
+        'dob' : 'october 8'
+    },
+    {
+        'f_name' : 'andrea',
+        'l_name' : 'waters',
+        'age' : 32,
+        'dob' : 'october 8'
+}
+
+]
+# output prints all the key: value of each dictionary
+# for one_student in students:
+#     print(one_student)
+
+#for one_student in students:
+    #print(one_student['f_name'])
+#outputs ronald, todd, tony, andrea
+
+# make a program that list out first name, last name, dob, age of every one 
+# of them 
+# for one_student in students:
+#     for key, value in one_student.items():
+#         print(key, value)
+
+# make a function that pulls all of the values from a single student
+# print(students[1]) # we took all info from tod 
+
+# # if you wanted only todd's info 
+# for key, value in students[1].items():
+#     print(key, value)
--- python/fundamentals/introduction/CH/week1/list_dict.py
+++ python/fundamentals/introduction/CH/week1/list_dict.py
@@ -9,10 +9,42 @@ some_list[2] = "ananas"
 #     print(item)
 
 Dallas = {
-    'f_name' : 'Dallas',
+    'f_name' : 'Dallas', # {key : value}
     'l_name' :'Beck',
     'age' : 23,
-    'dob' : 'august 15'
 }
 
-print(Dallas[0])
+# is this key of 'dob' is in Dallas, if it is
+# if 'dob' in Dallas:
+#     print(Dallas['dob'])
+# else:
+#       Dallas['dob'] = 'august 15' 
+
+# can we simplify this any further?
+# we can use a key word 'not' - as long as it's not in dallas
+# I want you to create it
+# other example ' if this is false do this' 
+if 'dob' not in Dallas:
+    Dallas['dob'] = 'august 15' 
+
+#Dallas['f_name'] = 'clark' # change the name value for this key
+# print(Dallas['age']) #output 23 
+print(Dallas)
+
+# for key in Dallas:
+#     print(key) # output 'f-name' 'l_name' 'age' 'dob'
+
+# for key in Dallas:
+#     if Dallas[key] == 'clark': # pulled only this specific key 
+#         print[key] # output 'f_name'
+
+# prefer way to loop through a dictionary, very clean
+# for key, value in Dallas.items():
+#     print(key, value) 
+# output 'f_name Dallas' 'l_name Beck' 'age 23' 'dob august 15'
+
+# what if i didn't have '.items()'
+for key in Dallas:
+    print(key, Dallas[key])
+
+
