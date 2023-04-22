37795708131ee53f670cdc6f97d9be33b118b5f2
Dalius Beck <daliusbeckxiv@gmail.com>
2023-04-22 16:24:56 -0700

    okay

--- python/flask/playground/server.py
+++ python/flask/playground/server.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index()
-    return 
+@app.route('/play/<times>')
+def index(times):
+    return render_template('index.html', times=times)
 
 
 
 
 
 if __name__ == '__main__':
-    app.run(debug = True, host="localhost", port=8002)
+    app.run(debug = True, host="localhost", port=5002)
