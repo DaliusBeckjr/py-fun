e76a4200f1d7c0a559b23e76acb8928d9bfa83b5
Dalius Beck <daliusbeckxiv@gmail.com>
2023-04-22 17:11:10 -0700

    wow

--- python/flask/playground/server.py
+++ python/flask/playground/server.py
@@ -2,14 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/home')
+def index():
+    return render_template('index.html')
 
-@app.route('/play/<times>')
-def index(times):
-    return render_template('index.html', times=times)
+@app.route('/play/<int:times>')
+def repeat(times):
+    return render_template('home.html', new_times=times)
 
 
 
 
 
 if __name__ == '__main__':
-    app.run(debug = True, host="localhost", port=5002)
+    app.run(debug = True, port=5002)
