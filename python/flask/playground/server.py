d3c892d62344b363c8574073c5d7d8bca43c68e5
Dalius Beck <daliusbeckxiv@gmail.com>
2023-04-23 10:19:12 -0700

    getting irritated

--- python/flask/playground/server.py
+++ python/flask/playground/server.py
@@ -2,15 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/home')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def hello_world():
+#     return render_template("index.html")
 
-@app.route('/play/<int:times>')
-def repeat(times):
+
+@app.route('/play')
+def bluebox(times=3):
     return render_template('home.html', new_times=times)
 
 
+@app.route('/play/<int:x>')
+def second(times, color='maroon'):
+    return render_template('home.html', new_times=x, color=color)
+
+
+@app.route('/play/<int:x>/<color>')
+def second(times, color):
+    return render_template('home.html', new_times=x, color=color)
 
 
 
