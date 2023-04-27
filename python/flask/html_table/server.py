e2e087d2d5bd6868db207eb21b62fc190ee268f7
Dalius Beck <daliusbeckxiv@gmail.com>
2023-04-26 22:46:39 -0700

    working on it

--- python/flask/html_table/server.py
+++ python/flask/html_table/server.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
-def index():
-    return render_template("index.html")
+def render_list():
+    # soon enough we'll get data from a database
+    users_info = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+        ]
+
+    return render_template("index.html", users_info= users_info)
+
+
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
