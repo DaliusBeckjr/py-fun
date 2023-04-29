34c469ae13a59377275d241749835a764d1ef655
Dalius Beck <daliusbeckxiv@gmail.com>
2023-04-29 12:35:36 -0700

    sos

--- python/flask/counter/server.py
+++ python/flask/counter/server.py
@@ -2,11 +2,25 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = 'keep it secret, keep it safe'
 
-@app.route('/count')
+@app.route('/')
 def root():
+    
+    # visits = session.get('visits') #retrieve a value from session
+    if 'visits' in session:
+        print('key exists!')
+        session['visits'] += 1 
+        
+    else:
+        print("key does NOT exist")
+        session['visits'] = 0
     return render_template('home.html')
 
 
+@app.route('/destroy_session')
+def delete_visits():
+    # session.clear()
+    session.pop('visits')  #clears a specific key
+    return redirect('/')
 
 
 
