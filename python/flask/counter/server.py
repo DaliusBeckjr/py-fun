d81b60c37062dc6e37930cb286f7111a3ec0e8e1
Dalius Beck <daliusbeckxiv@gmail.com>
2023-04-29 20:05:33 -0700

    it works now....

--- python/flask/counter/server.py
+++ python/flask/counter/server.py
@@ -2,32 +2,22 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = 'keep it secret, keep it safe'
 
+
 @app.route('/')
 def root():
-    
-    # visits = session.get('visits') #retrieve a value from session
-    if 'visits' in session:
-        print('key exists!')
-        session['visits'] += 1 
-        
-    else:
+    if "visits" not in session:
+        session["visits"] = 0
         print("key does NOT exist")
-        session['visits'] = 0
+    else:
+        session["visits"] +=1
+        print('key exists!')
     return render_template('home.html')
 
-@app.route('/add_one')
-def count_up():
-    session['visits'] += 1
+@app.route('/reset')
+def reset():
+    session.pop("visits", None) #clears a specific value
     return redirect('/')
-
-
-@app.route('/destroy_session')
-def delete_visits():
-    if 'visit' in session:
-        session.pop('visits', None)  #clears a specific key
-        return redirect('/')
-
-
+    
 
 
 if __name__ == "__main__":
